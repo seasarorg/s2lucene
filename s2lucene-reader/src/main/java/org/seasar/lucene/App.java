5a84878530378842c3709e81d4628b001dc6ea66
a-conv <a-conv@5243ab32-9249-44aa-b304-57b60a380331>
2007-03-10 13:37:16 +0000

    Inithial Import
    git-svn-id: https://www.seasar.org/svn/sandbox/s2lucene/trunk@5 5243ab32-9249-44aa-b304-57b60a380331

---
+++ s2lucene-reader/src/main/java/org/seasar/lucene/App.java
@@ -0,0 +1,13 @@
+package org.seasar.lucene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
